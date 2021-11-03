80c28fb85ad7540f40dd0e9e589f0b3098c43708
Alexey Natalin <mrreggies@gmail.com>
2021-11-03 14:09:43 +0700

    uefi-rs: added few doc comments for PCI I/O

--- src/proto/pci.rs
+++ src/proto/pci.rs
@@ -38,6 +38,7 @@ pub struct PciIO {
     rom_image: *const c_void,
 }
 
+/// Indicate appropriate I/O access size during memory-mapped I/O operations.
 pub trait ToIoWidth {
     const IO_WIDTH: IoWidth;
 }
@@ -54,9 +55,11 @@ impl ToIoWidth for u32 {
     const IO_WIDTH: IoWidth = IoWidth::U32;
 }
 
-// Note deriving Debug is only necessary so that we will
-// return Mapping back to the caller if it happened to fail
-// to unmap the address
+/// Representation of the bus relative memory address created by Map().
+/// TBD: capture lifetime of the system memory object.
+/// Note deriving Debug is only necessary so that we will
+/// return Mapping back to the caller if it happened to fail
+/// to unmap the address
 #[derive(Debug)]
 pub struct Mapping {
     addr: *const c_void,
@@ -65,68 +68,82 @@ pub struct Mapping {
 }
 
 impl Mapping {
+    /// Captured size of the system memory object.
     pub fn size(&self) -> usize {
         self.size
     }
 
+    /// Mapped bus relative address of the system memory object.
     pub fn device_address(&self) -> u64 {
         self.device_addr
     }
 
+    /// System memory address of the captured object.
     pub fn as_ptr(&self) -> *const c_void {
         self.addr
     }
 }
 
 impl PciIO {
+    /// Read PCI configuration space into a storage provided by a slice
     pub fn read_config<T: ToIoWidth>(&self, offset: u32, buffer: &mut [T]) -> Result {
         (self.config.read)(self, T::IO_WIDTH, offset, buffer.len(), buffer.as_mut_ptr().cast())
             .into()
     }
 
+    /// Read PCI configuration space into a storage provided by an object of size T
     pub fn read_config_single<T: ToIoWidth>(&self, offset: u32) -> Result<T> {
         let mut buffer: MaybeUninit<T> = MaybeUninit::uninit();
         (self.config.read)(self, T::IO_WIDTH, offset, 1, buffer.as_mut_ptr().cast())
             .into_with_val(|| unsafe { buffer.assume_init() })
     }
 
+    /// Write a number of objects into PCI configuration space
     pub fn write_config<T: ToIoWidth>(&self, offset: u32, buffer: &[T]) -> Result {
         (self.config.write)(self, T::IO_WIDTH, offset, buffer.len(), buffer.as_ptr().cast())
             .into()
     }
 
+    /// Read I/O port space region into a storage provided by a slice
     pub fn read_io<T: ToIoWidth>(&self, bar: IoRegister, offset: u64, buffer: &mut [T]) -> Result {
         (self.io.read)(self, T::IO_WIDTH, bar, offset, buffer.len(), buffer.as_mut_ptr().cast())
             .into()
     }
 
+    /// Read I/O port space region into an object storage
     pub fn read_io_single<T: ToIoWidth>(&self, bar: IoRegister, offset: u64) -> Result<T> {
         let mut buffer: MaybeUninit<T> = MaybeUninit::uninit();
         (self.io.read)(self, T::IO_WIDTH, bar, offset, 1, buffer.as_mut_ptr().cast())
             .into_with_val(|| unsafe { buffer.assume_init() })
     }
 
+    /// Write a number of objects into I/O port space region
     pub fn write_io<T: ToIoWidth>(&self, bar: IoRegister, offset: u64, buffer: &[T]) -> Result {
         (self.io.write)(self, T::IO_WIDTH, bar, offset, buffer.len(), buffer.as_ptr().cast())
             .into()
     }
 
+    /// Read memory-mapped I/O region into a storage provided by a slice
     pub fn read_mem<T: ToIoWidth>(&self, bar: IoRegister, offset: u64, buffer: &mut [T]) -> Result {
         (self.mem.read)(self, T::IO_WIDTH, bar, offset, buffer.len(), buffer.as_mut_ptr().cast())
             .into()
     }
 
+    /// Write an object into memory-mapped I/O region
     pub fn read_mem_single<T: ToIoWidth>(&self, bar: IoRegister, offset: u64) -> Result<T> {
         let mut buffer: MaybeUninit<T> = MaybeUninit::uninit();
         (self.mem.read)(self, T::IO_WIDTH, bar, offset, 1, buffer.as_mut_ptr().cast())
             .into_with_val(|| unsafe { buffer.assume_init() })
     }
 
+    /// Write number of objects into memory-mapped I/O region
     pub fn write_mem<T: ToIoWidth>(&self, bar: IoRegister, offset: u64, buffer: &[T]) -> Result {
         (self.mem.write)(self, T::IO_WIDTH, bar, offset, buffer.len(), buffer.as_ptr().cast())
             .into()
     }
 
+    /// Create bus relative memory address for DMA operation.
+    /// TBD: mark it unsafe
     pub fn map(&self, op: IoOperation, host_addr: *const c_void, num_bytes: usize) -> Result<Mapping> {
         let mut out_mapping = core::ptr::null();
         let mut out_num_bytes = num_bytes;
@@ -143,17 +160,20 @@ impl PciIO {
             })
     }
 
+    /// Remove device memory mapping for the previously mapped system address.
     pub fn unmap(&self, mapping: Mapping) -> Result<(), Mapping> {
         (self.unmap)(self, mapping.addr)
             .into_with_err(|_| mapping)
     }
 
+    /// Flushes all PCI controller specific transactions.
     pub fn flush(&self) -> Result {
         (self.flush)(self)
             .into()
     }
 }
 
+/// An index of the PCI Base Address Register.
 newtype_enum! {
     pub enum IoRegister: u8 => {
         R0 = 0,
@@ -166,10 +186,19 @@ newtype_enum! {
     }
 }
 
+/// Indicator of the upcoming bus master operation.
+/// The bus master is going to read or write to system memory.
+/// Or both.
 #[repr(i32)]
 pub enum IoOperation {
+    /// A read operation from system memory by a bus master.
     BusMasterRead,
+    /// A write operation to system memory by a bus master.
     BusMasterWrite,
+    /// Provides both read and write access to system memory
+    /// by both the processor and a bus master. The buffer
+    /// is coherent from both the processor’s and the bus
+    /// master’s point of view.
     BusMasterCommonBuffer
 }
 
